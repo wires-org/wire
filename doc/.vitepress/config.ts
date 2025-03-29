38fa4e4910c2674e4e1c58e52be3c97a4a00e951
marshmallow <marshycity@gmail.com>
2025-03-29 11:26:01 +1100

    docs: add installation steps & snippet check step

--- doc/.vitepress/config.ts
+++ doc/.vitepress/config.ts
@@ -1,82 +1,95 @@
 import { defineConfig } from 'vitepress'
 import pkg from "../package.json";
 import markdownItFootnote from 'markdown-it-footnote'
+import { groupIconMdPlugin, groupIconVitePlugin, localIconLoader } from 'vitepress-plugin-group-icons'
 
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    title: "wire",
-    description: "a tool to deploy nixos systems",
-    themeConfig: {
-        search: {
-            provider: 'local'
-        },
+	title: "wire",
+	description: "a tool to deploy nixos systems",
+	themeConfig: {
+		search: {
+			provider: 'local'
+		},
 
-        // https://vitepress.dev/reference/default-theme-config
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/wire' },
-            { text: 'Reference', link: '/reference/cli' },
-            {
-                text: pkg.version,
-                items: [
-                    {
-                        text: 'Changelog',
-                        link: 'https://github.com/wires-org/wire/blob/main/CHANGELOG.md'
-                    }
-                ]
-            }
-        ],
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Guide', link: '/guide/wire' },
+			{ text: 'Reference', link: '/reference/cli' },
+			{
+				text: pkg.version,
+				items: [
+					{
+						text: 'Changelog',
+						link: 'https://github.com/wires-org/wire/blob/main/CHANGELOG.md'
+					}
+				]
+			}
+		],
 
-        sidebar: {
-            '/guide/': [
-                {
-                    text: 'Introduction',
-                    items: [
-                        { text: 'What is Wire?', link: '/guide/wire' },
-                        { text: 'Getting Started', link: '/guide/getting-started' }
-                    ]
-                },
-                {
-                    text: 'Features',
-                    items: [
-                        { text: 'Parallelism', link: '/guide/parallelism' },
-                        { text: 'Secret management', link: '/guide/keys' },
-                        { text: 'Tagging', link: '/guide/tagging' },
-                        { text: 'hive.default', link: '/guide/hive-default' },
-                        { text: 'Magic Rollback', link: '/guide/magic-rollback' },
-                    ]
-                },
-                {
-                    text: 'Use cases',
-                    items: [
-                        { text: 'Tailscale', link: '/guide/tailscale' },
-                    ]
-                }
-            ],
-            '/reference/': [
-                {
-                    text: 'Reference',
-                    items: [
-                        { text: 'CLI', link: '/reference/cli' },
-                        { text: 'Module Options', link: '/reference/module' },
-                    ]
-                }
-            ]
-        },
+		sidebar: {
+			'/guide/': [
+				{
+					text: 'Introduction',
+					items: [
+						{ text: 'What is Wire?', link: '/guide/wire' },
+						{ text: 'Getting Started', link: '/guide/getting-started' }
+					]
+				},
+				{
+					text: 'Features',
+					items: [
+						{ text: 'Parallelism', link: '/guide/parallelism' },
+						{ text: 'Secret management', link: '/guide/keys' },
+						{ text: 'Tagging', link: '/guide/tagging' },
+						{ text: 'hive.default', link: '/guide/hive-default' },
+						{ text: 'Magic Rollback', link: '/guide/magic-rollback' },
+					]
+				},
+				{
+					text: 'Use cases',
+					items: [
+						{ text: 'Tailscale', link: '/guide/tailscale' },
+					]
+				}
+			],
+			'/reference/': [
+				{
+					text: 'Reference',
+					items: [
+						{ text: 'CLI', link: '/reference/cli' },
+						{ text: 'Module Options', link: '/reference/module' },
+					]
+				}
+			]
+		},
 
-        editLink: {
-            pattern: 'https://github.com/wires-org/wire/edit/main/docs/:path',
-            text: 'Edit this page on GitHub'
-        },
+		editLink: {
+			pattern: 'https://github.com/wires-org/wire/edit/main/docs/:path',
+			text: 'Edit this page on GitHub'
+		},
 
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/wires-org/wire' }
-        ]
-    },
-    markdown: {
-        config: (md) => {
-            md.use(markdownItFootnote)
-        }
-    },
+		socialLinks: [
+			{ icon: 'github', link: 'https://github.com/wires-org/wire' }
+		]
+	},
+	markdown: {
+		config: (md) => {
+			md.use(markdownItFootnote);
+			md.use(groupIconMdPlugin);
+		}
+	},
+	vite: {
+		plugins: [
+			groupIconVitePlugin({
+				customIcon: {
+					'nixos': 'vscode-icons:file-type-nix',
+					'configuration.nix': 'vscode-icons:file-type-nix',
+					'home': localIconLoader(import.meta.url, '../assets/homemanager.svg'),
+				}
+			})
+		]
+	},
 })
--- doc/guide/getting-started.md
+++ doc/guide/getting-started.md
@@ -1,1 +1,14 @@
 # Getting Started
+
+## Installation
+
+### NixOS / Home Manager
+
+::: code-group
+
+<<< @/snippets/getting-started/nixos.flake.nix [flake.nix (NixOS)]
+<<< @/snippets/getting-started/hm.flake.nix [flake.nix (Home Manager)]
+<<< @/snippets/getting-started/configuration.nix
+<<< @/snippets/getting-started/home.nix
+
+:::
