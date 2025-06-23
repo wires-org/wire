c78b24e8766317c1185e1658de1196259302dfdc
marshmallow <marshycity@gmail.com>
2025-06-24 08:36:20 +1000

    test --reboot and wait until node reappears

--- tests/nix/suite/test_remote_deploy/default.nix
+++ tests/nix/suite/test_remote_deploy/default.nix
@@ -10,13 +10,34 @@
       deployer_so = collect_store_objects(deployer)
       receiver_so = collect_store_objects(receiver)
 
-      deployer.fail(f"wire apply --on receiver-unreachable --no-progress --path {TEST_DIR}/hive.nix --no-keys -vvv >&2")
+      with subtest("Test unreachable hosts"):
+        deployer.fail(f"wire apply --on receiver-unreachable --no-progress --path {TEST_DIR}/hive.nix --no-keys -vvv >&2")
 
-      deployer.succeed(f"wire apply --on receiver --no-progress --path {TEST_DIR}/hive.nix --no-keys -vvv >&2")
+      with subtest("Check basic apply"):
+          deployer.succeed(f"wire apply --on receiver --no-progress --path {TEST_DIR}/hive.nix --no-keys -vvv >&2")
 
-      receiver.wait_for_unit("sshd.service")
+          identity = receiver.succeed("cat /etc/identity")
+          assert identity == "first", "Identity of first apply wasn't as expected"
 
-      receiver.succeed("test -f /etc/a")
+      with subtest("Check boot apply"):
+        first_system = receiver.succeed("readlink -f /run/current-system")
+
+        deployer.succeed(f"wire apply boot --on receiver-second --no-progress --path {TEST_DIR}/hive.nix --no-keys -vvv >&2")
+
+        _first_system = receiver.succeed("cat /etc/identity")
+        assert first_system == _first_system, "apply boot without --rebot changed /run/current-system"
+
+      with subtest("Check /etc/identity after reboot"):
+        receiver.reboot()
+
+        identity = receiver.succeed("cat /etc/identity")
+        assert identity == "second", "Identity didn't change after second apply"
+
+      with subtest("Check --reboot"):
+        deployer.succeed(f"wire apply boot --on receiver-third --no-progress --path {TEST_DIR}/hive.nix --reboot --no-keys -vvv >&2")
+
+        identity = receiver.succeed("cat /etc/identity")
+        assert identity == "third", "Identity didn't change after third apply"
     '';
   };
 }
